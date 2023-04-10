cc0be9f4642673ab51a369cdd4e623a88ec9cd9a
anshul45 <anshulgarwal45@gmail.com>
2023-04-10 20:14:17 +0530

    commit

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,7 +1,25 @@
 import React from "react";
+import { Container, Row, Col } from "reactstrap";
+import logo from "../../assets/images/res-logo.png";
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <footer className="footer">
+      <Container>
+        <Row>
+          <Col lg="3" md="4" sm="6">
+            <div className="logo">
+              <img src={logo} alt="logo" />
+              <h5>Tasty Treat</h5>
+            </div>
+          </Col>
+          <Col lg="3" md="4" sm="6"></Col>
+          <Col lg="3" md="4" sm="6"></Col>
+          <Col lg="3" md="4" sm="6"></Col>
+        </Row>
+      </Container>
+    </footer>
+  );
 };
 
 export default Footer;
