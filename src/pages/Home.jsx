34c077c57dbaef0cd7fdf658dbe368b2e7eddbf9
anshul45 <anshulgarwal45@gmail.com>
2023-04-17 02:07:26 +0530

    created home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 
 import Helmet from "../components/Helmet/Helmet";
-import { Container, Row, Col } from "reactstrap";
+import { Container, Row, Col, ListGroupItem, ListGroup } from "reactstrap";
 
 import heroImg from "../assets/images/hero.png";
 import "../styles/hero-section.css";
@@ -15,13 +15,17 @@ import featureImg01 from "../assets/images/service-01.png";
 import featureImg02 from "../assets/images/service-02.png";
 import featureImg03 from "../assets/images/service-03.png";
 
-import products from "../assets/fake-data/products";
+import products from "../assets/fake-data/products.js";
 
 import foodCategoryImg01 from "../assets/images/hamburger.png";
 import foodCategoryImg02 from "../assets/images/pizza.png";
 import foodCategoryImg03 from "../assets/images/bread.png";
 
 import ProductCard from "../components/UI/product-card/ProductCard";
+
+import whyImg from "../assets/images/location.png";
+
+import networkImg from "../assets/images/network.png";
 
 const featureData = [
   {
@@ -42,11 +46,18 @@ const featureData = [
 ];
 
 const Home = () => {
-  const [category, setCategory] = useState("All");
+  const [category, setCategory] = useState("ALL");
   const [allProducts, setAllProducts] = useState(products);
+  const [hotPizza, setHotPizza] = useState([]);
 
   useEffect(() => {
-    if (category === "All") {
+    const filteredPizza = products.filter((item) => item.category === "Pizza");
+    const slicePizza = filteredPizza.slice(0, 4);
+    setHotPizza(slicePizza);
+  }, [hotPizza]);
+
+  useEffect(() => {
+    if (category === "ALL") {
       setAllProducts(products);
     }
 
@@ -54,6 +65,7 @@ const Home = () => {
       const filteredProducts = products.filter(
         (item) => item.category === "Burger"
       );
+
       setAllProducts(filteredProducts);
     }
 
@@ -61,14 +73,18 @@ const Home = () => {
       const filteredProducts = products.filter(
         (item) => item.category === "Pizza"
       );
+
       setAllProducts(filteredProducts);
     }
+
     if (category === "BREAD") {
       const filteredProducts = products.filter(
         (item) => item.category === "Bread"
       );
+
       setAllProducts(filteredProducts);
     }
+    // eslint-disable-next-line
   }, [category]);
 
   return (
@@ -168,27 +184,35 @@ const Home = () => {
             <Col lg="12">
               <div className="food_category d-flex align-items-center justify-content-center gap-4">
                 <button
-                  className="all__btn foodBtnActive"
-                  onClick={() => setCategory("All")}
+                  className={`all__btn ${
+                    category === "ALL" ? "foodBtnActive" : ""
+                  }`}
+                  onClick={() => setCategory("ALL")}
                 >
                   All
                 </button>
                 <button
-                  className="d-flex align-items-center gap-2"
+                  className={`d-flex align-items-center gap-2 ${
+                    category === "BURGER" ? "foodBtnActive" : ""
+                  }`}
                   onClick={() => setCategory("BURGER")}
                 >
                   <img src={foodCategoryImg01} alt="" />
                   Burger
                 </button>
                 <button
-                  className="d-flex align-items-center gap-2"
+                  className={`d-flex align-items-center gap-2 ${
+                    category === "PIZZA" ? "foodBtnActive" : ""
+                  }`}
                   onClick={() => setCategory("PIZZA")}
                 >
                   <img src={foodCategoryImg02} alt="" />
                   Pizza
                 </button>
                 <button
-                  className="d-flex align-items-center gap-2"
+                  className={`d-flex align-items-center gap-2 ${
+                    category === "BREAD" ? "foodBtnActive" : ""
+                  }`}
                   onClick={() => setCategory("BREAD")}
                 >
                   <img src={foodCategoryImg03} alt="" />
@@ -196,11 +220,99 @@ const Home = () => {
                 </button>
               </div>
             </Col>
-            {products.map((item) => (
+            {allProducts.map((item) => (
               <Col lg="3" md="4" key={item.id} className="mt-5">
                 <ProductCard item={item} />
               </Col>
             ))}
+          </Row>
+        </Container>
+      </section>
+      <section>
+        <Container>
+          <Row>
+            <Col lg="6" md="6">
+              <img src={whyImg} alt="why-tasty-treat" className="w-100" />
+            </Col>
+            <Col lg="6" md="6">
+              <div className="why__tasty-treat">
+                <h2 className="tasty__treat-title mb-4">
+                  Why <span>Tasty Treat?</span>
+                </h2>
+                <p className="tasty__treat-desc">
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                  Similique assumenda delectus recusandae, nam, soluta dicta
+                  quibusdam, quos deleniti pariatur quis culpa maiores
+                </p>
+                <ListGroup className="mt-5">
+                  <ListGroupItem className="border-0 ps-0">
+                    <p className="choose__us-title d-flex align-items-center gap-2">
+                      <i class="ri-checkbox-circle-line"></i>Fresh and tasty
+                      foods
+                    </p>
+                    <p className="choose__us-desc">
+                      Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                      Rem, vitae.
+                    </p>
+                  </ListGroupItem>
+                  <ListGroupItem className="border-0 ps-0">
+                    <p className="choose__us-title d-flex align-items-center gap-2">
+                      <i class="ri-checkbox-circle-line"></i>Quality support
+                    </p>
+                    <p className="choose__us-desc">
+                      Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                      Minima, ipsum.
+                    </p>
+                  </ListGroupItem>
+                  <ListGroupItem className="border-0 ps-0">
+                    <p className="choose__us-title d-flex align-items-center gap-2">
+                      <i class="ri-checkbox-circle-line"></i>Order from any
+                      location
+                    </p>
+                    <p className="choose__us-desc">
+                      Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                      Minima, ipsum.
+                    </p>
+                  </ListGroupItem>
+                </ListGroup>
+              </div>
+            </Col>
+          </Row>
+        </Container>
+      </section>
+      <section className="pt-0">
+        <Container>
+          <Row>
+            <Col lg="12" className="text-center mb-5">
+              <h2>Hot Pizza</h2>
+            </Col>
+            {hotPizza.map((item) => (
+              <Col lg="3" md="4" key={item.id}>
+                <ProductCard item={item} />
+              </Col>
+            ))}
+          </Row>
+        </Container>
+      </section>
+      <section>
+        <Container>
+          <Row>
+            <Col lg="6" md="6">
+              <div className="testimonial">
+                <h5 className="testimonial__subtitle mb-4">Testimonial</h5>
+                <h2 className="testimonial__title mb-4">
+                  What our <span>customers</span> are saying
+                </h2>
+                <p className="testimonial__desc">
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio
+                  non ipsum quo iusto dolores aliquam sapiente natus! Odio,
+                  deserunt nihil?
+                </p>
+              </div>
+            </Col>
+            <Col lg="6" md="6">
+              <img src={networkImg} alt="testimonail-img" className="w-100" />
+            </Col>
           </Row>
         </Container>
       </section>
