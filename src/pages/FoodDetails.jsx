a3ca19ec43a627f95797984e739e2f32adc38409
anshul45 <anshulgarwal45@gmail.com>
2023-04-25 22:08:55 +0530

    fo0d detail component

--- src/pages/FoodDetails.jsx
+++ src/pages/FoodDetails.jsx
@@ -1,7 +1,41 @@
 import React from "react";
-
+import products from "../assets/fake-data/products";
+import { useParams } from "react-router-dom";
+import Helmet from "../components/Helmet/Helmet";
+import CommonSection from "../components/UI/common-section/CommonSection";
+import { Container, Row, Col } from "reactstrap";
+import productImg from "../assets/images/product_01.1.jpg";
 const FoodDetails = () => {
-  return <div>FoodDetails</div>;
+  return (
+    <Helmet title="Product-details">
+      <CommonSection title="product 01" />
+      <section>
+        <Container>
+          <Row>
+            <Col lg="2" md="2">
+              <div className="product__images">
+                <div className="img__item">
+                  <img src={productImg} alt="" className="w-50" />
+                </div>
+                <div className="img__item">
+                  <img src={productImg} alt="" className="w-50" />
+                </div>
+                <div className="img__item">
+                  <img src={productImg} alt="" className="w-50" />
+                </div>
+              </div>
+            </Col>
+            <Col lg="4" md="4">
+              <div className="product__main-img">
+                <img src={productImg} alt="" className="w-100" />
+              </div>
+            </Col>
+            <Col lg="6" md="6"></Col>
+          </Row>
+        </Container>
+      </section>
+    </Helmet>
+  );
 };
 
 export default FoodDetails;
